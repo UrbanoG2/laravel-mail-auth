d3239a384101442096ead7faab54ef5a708613c0
UrbanoG2 <giuseppe.16.urbano@gmail.com>
2022-03-14 17:21:40 +0100

    emails working

---
+++ app/Http/Controllers/Api/ContactController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\lead;
+use App\Mail\SendNewMail;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
+
+
+class ContactController extends Controller
+{
+
+
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        } else {
+            //salvare la mail nel DB
+            $new_lead = new Lead();
+            $new_lead->fill($data);
+            $new_lead->save();
+
+            //inviare la mail ad admin
+            Mail::to('admin@toporagno.com')->send(new SendNewMail($new_lead));
+
+            //inviare la mail al guest
+            // Mail::to($new_lead->email)->send(new SendNewMailGuest($new_lead));
+
+            return response()->json([
+                'success' => true
+            ]);
+        }
+    }
+}
